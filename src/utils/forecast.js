ee958c3449635ff1ac41f3ad3b6d8495b153c49b
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-05-17 09:35:02 +0200

    add error logic

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,6 +6,8 @@ const forecast = (latitude, longitude, callback) => {
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
