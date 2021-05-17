0370afd0750f93e729c902576cf4117002651182
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-05-17 09:31:36 +0200

    added humidity to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=4b61e6a91b64af33610ebc9c5c09505b&query=${latitude},${longitude}&units=f`;
+  const url = `http://api.weatherstack.com/current?access_key=4b61e6a91b64af33610ebc9c5c09505b&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity outside is ${body.current.humidity}.`
       );
     }
   });
